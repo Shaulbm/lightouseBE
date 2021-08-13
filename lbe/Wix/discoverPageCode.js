a1107726b11e81770fe05451e481e2c07990f511
Shaulbm <shaul.ben.maor@gmail.com>
2021-08-13 18:15:14 +0300

    Select subordinates for discovery

--- lbe/Wix/discoverPageCode.js
+++ lbe/Wix/discoverPageCode.js
@@ -17,13 +17,14 @@ const timeManagementIssue = "427e1335-5a0f-45a2-8c3f-3139f98452df";
 const technicalManagerialBalanceIssue = "5631dfbc-65a7-431a-b08f-456058bbddf7";
 const leadingIssue = "dabe54b7-47f3-4ed9-a02c-11868f3d94d1";
 
-$w.onReady(function () {
+$w.onReady(async function () {
 	// Write your JavaScript here
 
 	// To select an element by ID use: $w("#elementID")
 	hideIssuesOption();
 	$w("#OpenDiscovery").show();
 
+	await fillOrgData();
 	// Click "Preview" to run your code
 });
 
@@ -146,3 +147,54 @@ export async function OpenDiscovery_click(event) {
 	showIssuesOption();
 	$w("#selectedIssue").show();
 }
+
+export async function fillOrgData()
+{
+	const orgData = await fetchOrgData();
+	if (orgData.length > 0)
+	{
+		let user = wixUsers.currentUser;
+		const userMail = await user.getEmail()
+		$w("#userSelection").show();
+
+		console.log ("adding items to selection");
+		let orgDataStr = "";
+		let opts = $w("#userSelection").options;
+		opts = [{"label": "Me", "value": userMail}];
+
+		for (let currentOrgMemeberIdx = 0; currentOrgMemeberIdx < orgData.length; currentOrgMemeberIdx++)
+		{
+			opts.push({"label": orgData[currentOrgMemeberIdx].name, "value": orgData[currentOrgMemeberIdx].name});
+		}
+
+		$w("#userSelection").options = opts;
+		//$w("#userSelection").selectedIndex = 0;
+	}
+	else
+	{
+		$w("#userSelection").hide();
+	}
+}
+
+export async function fetchOrgData()
+{
+	let user = wixUsers.currentUser;
+	let userMail = "Undefined";
+
+	if (user.loggedIn)
+	{
+		userMail = await user.getEmail();
+		console.log("user id is: " + user.id + " user email is: " + userMail);
+	}
+
+	const url = "https://52.30.104.65:8000/usersUnder?userName=" + userMail;
+	console.log("url to API:" + url);
+
+	const httpResponse = await fetch(url,{'method':'POST'});
+    if (httpResponse.ok){
+        const result = await httpResponse.json();
+		console.log ("got user details response"+ JSON.stringify(result));
+        return result;
+    }
+    return {};
+ }
