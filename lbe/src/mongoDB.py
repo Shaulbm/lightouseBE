be6d25525bd1b9f2e130fc2eb229d5e8796ffb48
Shaulbm <shaul.ben.maor@gmail.com>
2021-10-17 21:00:49 +0300

    fix

--- lbe/src/mongoDB.py
+++ lbe/src/mongoDB.py
@@ -4,7 +4,7 @@ from pymongo import MongoClient
 import pymongo
 import json
 from motivationsData import motivationData
-from generalData import textData
+from generalData import textData, userData
 from singleton import Singleton
 
 LOCALE_HEB_MA = 1
@@ -70,74 +70,38 @@ class moovDBInstance(metaclass=Singleton):
         else:
             # this is a new motivation
             dataCollection.insert_one(textDataObj.toJSON())
+    
+    def insertOrUpdateUser (self, currUserData):
+        userDataJSON = currUserData.toJSON()
 
-def insertMotivation():
-    moovDB = moovDBInstance()
-    db = moovDB.getDatabase()
-    motivationsCollection = db["motivationsTest"]
+        db = self.dataBaseInstance()
+        usersCollection = db["users"]
 
-    heb_ma_LocaleCollection = db["locale_he_ma"]
-    heb_fe_LocaleCollection = db["locale_he_fe"]
-    eng_LocaleCollection = db["locale_en"]
+        foundUser = usersCollection.find_one({"id":currUserData.id})
 
-    newMotivation = motivationData()
-    newMotivation.id = "M01"
-    newMotivation.name = "M01_1"
-    newMotivation.shortDescription = "M01_2"
-    newMotivation.longDescription = "M01_3"
-    newMotivation.tailResolution = "M01_4"
-    newMotivation.imageUrl = "url"
+        if (foundUser is not None):
+            #the user already exists - update the user
+            userDataFilter = {"id" : currUserData.id}
+            usersCollection.replace_one(userDataFilter, currUserData.toJSON())
+        else:
+            #this is a new user
+            usersCollection.insert_one(currUserData.toJSON())
 
-    currentTextData = textData("M01", "M01_1", "Autonomy")
-    eng_LocaleCollection.insert_one(currentTextData.toJSON())
 
-    currentTextData = textData("M01", "M01_1", "אוטונומיה")
-    heb_ma_LocaleCollection.insert_one(currentTextData.toJSON())
-    heb_fe_LocaleCollection.insert_one(currentTextData.toJSON())
+    def getMotivation (self, id, locale):
+        db = self.getDatabase()
+        motivationCollection = db["motivationsTest"]
 
-    currentTextData = textData ("M01", "M01_2", "english short description here")
-    eng_LocaleCollection.insert_one(currentTextData.toJSON())
+        motivationDataJSON = motivationCollection.find_one({"id" : id})
+        print ("motivation data is {0}", motivationDataJSON)
 
-    currentTextData = textData ("M01", "M01_2", "חייב להיות לך מרחב להחליט ולפעול בדרך שבחרת, ושאחרים לא יתערבו ולא יכתיבו לך מה ואיך לעשות.")
-    heb_ma_LocaleCollection.insert_one(currentTextData.toJSON())
+        motivationTextsDic = getTextDataByParent(id, locale)
 
-    currentTextData = textData ("M01", "M01_2","את צריכה לעשות את הדברים בדרך שלך, ומבלי שאחרים יתערבו וודאי מבלי שאחרים יכתיבו לך מה ואיך לעשות")
-    heb_fe_LocaleCollection.insert_one(currentTextData.toJSON())
+        newMotivtion = motivationData()
+        newMotivtion.buildFromJSON(motivationDataJSON, motivationTextsDic)
 
-    currentTextData = textData ("M01", "M01_3", "english long description here")
-    eng_LocaleCollection.insert_one(currentTextData.toJSON())
-
-    currentTextData = textData ("M01", "M01_3", "אתה במיטבך כשיש לך מרחב להחליט ולפעול. אתה צריך לעשות את הדברים בדרך שלך, ומבלי שאחרים יתערבו וודאי מבלי שאחרים יכתיבו לך מה ואיך לעשות. חופש לפעול ועצמאות בקבלת ההחלטות הם הכרח עבורך. אתה לא אוהב לפעול על פי כללים ונהלים, ודאי לא אלה שלא אתה קבעת לעצמך. בכלל, קרוב לודאי שעבודה לבד מועדפת עלייך. זה כמובן לא אומר שאתה לא עובד או לא יכול לעבוד עם אחרים, אלא שכשהחלטות לא נמצאות באופן מלא בידיים שלך, אתה חש שאתה לא במיטבך.")
-    heb_ma_LocaleCollection.insert_one(currentTextData.toJSON())
-
-    currentTextData = textData ("M01", "M01_3","את במיטבך כשיש לך מרחב להחליט ולפעול. את צריכה לעשות את הדברים בדרך שלך, ומבלי שאחרים יתערבו וודאי מבלי שאחרים יכתיבו לך מה ואיך לעשות. חופש לפעול ועצמאות בקבלת ההחלטות הם הכרח עבורך. את לא אוהבת לפעול על פי כללים ונהלים, ודאי לא אלה שלא את קבעת לעצמך. בכלל, קרוב לודאי שעבודה לבד מועדפת עלייך. זה כמובן לא אומר שאת לא עובדת או לא יכולה לעבוד עם אחרים, אלא שכשהחלטות לא נמצאות באופן מלא בידיים שלך, את חשה שאת לא במיטבך.")
-    heb_fe_LocaleCollection.insert_one(currentTextData.toJSON())
-
-    currentTextData = textData ("M01", "M01_4", "autonnomy tail resolution phrase")
-    eng_LocaleCollection.insert_one(currentTextData.toJSON())
-
-    currentTextData = textData ("M01", "M01_4", "לקבל החלטות בדרך שלי")
-    heb_ma_LocaleCollection.insert_one(currentTextData.toJSON())
-
-    currentTextData = textData ("M01", "M01_4","לקבל החלטות בדרך שלי")
-    heb_fe_LocaleCollection.insert_one(currentTextData.toJSON())
-
-    motivationsCollection.insert_one (newMotivation.toJSON())
-
-def getMotivation (id, locale):
-    moovDB = moovDBInstance()
-    db = moovDB.getDatabase()
-    motivationCollection = db["motivationsTest"]
-
-    motivationDataJSON = motivationCollection.find_one({"id" : id})
-    print ("motivation data is {0}", motivationDataJSON)
-
-    motivationTextsDic = getTextDataByParent(id, locale)
-
-    newMotivtion = motivationData()
-    newMotivtion.buildFromJSON(motivationDataJSON, motivationTextsDic)
-
-    print ("motivation object is {0}", newMotivtion.toJSON())
+        # print ("motivation object is {0}", newMotivtion.toJSON())
+        return newMotivtion 
 
 
 #insertMotivation()
