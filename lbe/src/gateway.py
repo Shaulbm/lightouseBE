1a7b10839fb283c97bae9bcbe6fa8b73e34610c6
Shaulbm <shaul.ben.maor@gmail.com>
2021-10-17 20:52:58 +0300

    added get motivation to the API

--- lbe/src/gateway.py
+++ lbe/src/gateway.py
@@ -1,76 +1,84 @@
 from fastapi import Header, APIRouter
 from fastapi import HTTPException
+from lbe.src.motivationsData import motivationDecoder
 from users import UsersLogic
+from mongoDB import moovDBInstance
 
 router = APIRouter()
 users = UsersLogic()
 
-@router.post("/registerUser")
-def register_user(user):
-    userDetails = users.registerUser(userName=user)
-    return userDetails
+@router.get("/motivation")
+def get_motivation(id, locale):
+    dbActions = moovDBInstance()
+    motivationDetails = dbActions.getMotivation (id,locale)
 
-@router.get("/createTrainingData")
-def create_training_data():
-    return None #do nothing
-    users.createTrainingData()
+    return motivationDetails
+# @router.post("/registerUser")
+# def register_user(user):
+#     userDetails = users.registerUser(userName=user)
+#     return userDetails
+
+# @router.get("/createTrainingData")
+# def create_training_data():
+#     return None #do nothing
+#     users.createTrainingData()
     
-@router.post("/getTrainingStage")
-def get_training_stage(issueId, stage):
-    trainingStageDetails = users.getTrainingStage(issueId, stage)
-    return trainingStageDetails
+# @router.post("/getTrainingStage")
+# def get_training_stage(issueId, stage):
+#     trainingStageDetails = users.getTrainingStage(issueId, stage)
+#     return trainingStageDetails
 
-@router.post("/getCourseLesson")
-def get_course_lesson(issueId, lesson):
-    courselessonDetails = users.getCourseLessonData(issueId, lesson)
-    return courselessonDetails
+# @router.post("/getCourseLesson")
+# def get_course_lesson(issueId, lesson):
+#     courselessonDetails = users.getCourseLessonData(issueId, lesson)
+#     return courselessonDetails
 
-@router.post("/issue")
-def get_issue(id):
-    issueDetails = users.getIssueData(id)
-    return issueDetails
+# @router.post("/issue")
+# def get_issue(id):
+#     issueDetails = users.getIssueData(id)
+#     return issueDetails
 
-@router.post("/trainingMap")
-def get_training_map(issueId, stage):
-    trainingMapDetails = users.getTrainingMap(issueId, stage)
-    return trainingMapDetails
+# @router.post("/trainingMap")
+# def get_training_map(issueId, stage):
+#     trainingMapDetails = users.getTrainingMap(issueId, stage)
+#     return trainingMapDetails
 
-@router.post("/setUserIssueId")
-def set_user_issue_id(userName, issueId):
-    userDetails = users.setUserCurrIssueId(userName, issueId)
-    return userDetails
+# @router.post("/setUserIssueId")
+# def set_user_issue_id(userName, issueId):
+#     userDetails = users.setUserCurrIssueId(userName, issueId)
+#     return userDetails
 
-@router.post("/setUserNextTrainingStage")
-def set_user_next_training_stage(userName):
-    userDetails = users.setUserNextTrainingStage(userName)
-    return userDetails
+# @router.post("/setUserNextTrainingStage")
+# def set_user_next_training_stage(userName):
+#     userDetails = users.setUserNextTrainingStage(userName)
+#     return userDetails
 
-@router.post("/setUserNextCourseLesson")
-def set_user_next_course_lesson(userName):
-    userDetails = users.setUserNextCourseLesson(userName)
-    return userDetails
+# @router.post("/setUserNextCourseLesson")
+# def set_user_next_course_lesson(userName):
+#     userDetails = users.setUserNextCourseLesson(userName)
+#     return userDetails
 
-@router.post("/additionalIssueDetails")
-def additional_issue_details(issueId):
-    additionalIssueDetails = users.getIssueAdditionalData(issueId)
-    return additionalIssueDetails
+# @router.post("/additionalIssueDetails")
+# def additional_issue_details(issueId):
+#     additionalIssueDetails = users.getIssueAdditionalData(issueId)
+#     return additionalIssueDetails
 
-@router.post("/questionDetails")
-def question_details(questionId):
-    questionDetails = users.getQuestionDetails(questionId)
-    return questionDetails
+# @router.post("/questionDetails")
+# def question_details(questionId):
+#     questionDetails = users.getQuestionDetails(questionId)
+#     return questionDetails
 
-@router.post("/respondQuestion")
-def question_response(questionId, userId, response):
-    questionDetails = users.respondQuestion(questionId, userId, response)
-    return questionDetails
+# @router.post("/respondQuestion")
+# def question_response(questionId, userId, response):
+#     questionDetails = users.respondQuestion(questionId, userId, response)
+#     return questionDetails
 
-@router.post("/userManager")
-def get_manager_details(userName):
-    managerDetails = users.getManagerDetails(name = userName)
-    return managerDetails
+# @router.post("/userManager")
+# def get_manager_details(userName):
+#     managerDetails = users.getManagerDetails(name = userName)
+#     return managerDetails
 
-@router.post("/usersUnder")
-def get_users_under(userName):
-    usersList = users.getUsersUnder(userName)
-    return usersList
+# @router.post("/usersUnder")
+# def get_users_under(userName):
+#     usersList = users.getUsersUnder(userName)
+#     return usersList
--- lbe/src/generalData.py
+++ lbe/src/generalData.py
@@ -17,8 +17,34 @@ class textData:
     def fromJSON (self, jsonData):
         try:
             self.parentId = jsonData["parentId"]
-            self.id = jsonData["textId"]
+            self.id = jsonData["id"]
             self.contentText = jsonData["contentText"]
         except Exception as err:
             #log this
             raise TypeError(str.format("failed to load text data from JSON, data is {0}, error is {1}", jsonData, err))
+
+class userData:
+    def __init__(self, id = "", parentId = "", mailAddress = "", motivations = None):
+        self.id = id
+        self.parentId = parentId
+        self.mailAddress = mailAddress
+
+        if (motivations is not None):
+            self.motivations = motivations.copy()
+
+    def toJSON (self):
+        userDataJSON = jsonpickle.encode(self, unpicklable=False)
+
+        jsonObject = json.loads (userDataJSON)
+
+        return jsonObject 
+
+    def fromJSON (self, jsonData):
+        self.id = jsonData["id"]
+        self.parentId = jsonData["parentId"]
+        self.mailAddress = jsonData["mailAddress"]
+
+        if (len(jsonData["motivations"]) > 0):
+            self.motivations = json.loads(jsonData["motivations"])
+        else:
+            self.motivations = {}
