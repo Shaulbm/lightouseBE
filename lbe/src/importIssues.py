e17ea4effb74d1ae1379629ee3a8371b23f794cc
Shaulbm <shaul.ben.maor@gmail.com>
2021-11-17 15:33:52 +0200

    import issues

--- lbe/src/importIssues.py
+++ lbe/src/importIssues.py
@@ -15,10 +15,10 @@ SCOPES = ['https://www.googleapis.com/auth/spreadsheets', "https://www.googleapi
 
 # The ID and range of a sample spreadsheet.
 SAMPLE_SPREADSHEET_ID = '1c68_oJr28b6USdkj3CW5h9KaXvLKpB5iCACJoKygEs0'
-ISSUES_RANGE_NAME = 'Issues!A1:K2'
+ISSUES_RANGE_NAME = 'Issues!A1:K4'
 SUBJECTS_RANGE_NAME = 'Subjects!A1:G4'
-RESOLVING_MOTIVATIONS_RANGE_NAME = 'IssueResolvingMotivations!A1:G8'
-CONTRIBUTING_MOTIVATIONS_RANGE_NAME = 'IssueContributingMotivations!A1:G7'
+RESOLVING_MOTIVATIONS_RANGE_NAME = 'IssueResolvingMotivations!A1:G22'
+CONTRIBUTING_MOTIVATIONS_RANGE_NAME = 'IssueContributingMotivations!A1:G19'
 
 def main():
     """Shows basic usage of the Sheets API.
